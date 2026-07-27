a809a3008bf122a31153a4db9da10c4257546887
Gilbert Bouic <22957074+gilbertbouic@users.noreply.github.com>
2026-07-27 08:43:36 +0400

    Bump to v2.1.1 with rebranded launcher icons.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.culturalassimilation.manual"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
